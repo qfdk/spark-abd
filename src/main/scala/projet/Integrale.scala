59fa4bffa1afde474026887291b191e19f83666d
qfdk <qfdk@vip.qq.com>
2016-10-04 22:12:19 +0200

    changed integrale

--- src/main/scala/projet/Integrale.scala
+++ src/main/scala/projet/Integrale.scala
@@ -6,21 +6,21 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object Integrale {
-	def main(args: Array[String]) {
-		val conf = new SparkConf().setAppName("Integrale")
-				val sc = new SparkContext(conf)
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Integrale")
+    val sc = new SparkContext(conf)
+  }
 
+  def calcule(min: Int, max: Int, slice: Int, sc: SparkContext): Double = {
 
-	}
+    val pas = (max - min) * 1.0 / slice
 
-	def calcule(min:Int,max:Int,slice:Int,sc:SparkContext):Double={
+    val inte = sc.parallelize(1 until slice, 1)
+      .map { i =>
+        pas * 1.0 / i
+      }.reduce(_ + _)
 
-			val pas=(max-min)*1.0/slice 
-
-					val inte = sc.parallelize(1 until slice, 1).map { i =>
-					pas*1.0/i
-			}.reduce(_+_)
-
-			inte
-	}
+    inte
+  }
+  
 }
